4ef7c7f72e37978604bb67f9a4cefd26fc241b7c
Allen <allenyangyl@gmail.com>
2016-12-05 00:37:56 -0500

    define proto.hpp

--- include/protos.hpp
+++ include/protos.hpp
@@ -2,15 +2,21 @@
 #define PROTOS_HPP_
 
 struct FeaturePoint {
-  
+  Eigen::Vector2d pos;
+  Eigen::Vector3i rgb;
 }
 
 struct FeatureStruct {
- 
+  std::vector<FeaturePoint> feature_point;
+  std::SparseMatrix<std::Complex<double>, Eigen::RowMajor> feature_idx;
+  std::SparseMatrix<std::Complex<double>, Eigen::RowMajor> skeleton;
 }
 
 struct Graph {
- public:
+  std::vector<int> frame_idx;
+  std::vector<Eigen::Matrix<double, 3, 3, Eigen::ColMajor>> K;
+  std::vector<Eigen::Matrix<double, 3, 4, Eigen::ColMajor>> Mot;
+  Eigen::Matrix<double, 3, Eigen::Dynamic, Eigen::ColMajor> Str;
 }
 
 #endif
