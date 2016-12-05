1c297176e16f3beebb46619b01d98da2fb756272
rangozhang <rangozhangyu@gmail.com>
2016-12-05 18:18:02 -0500

    fix bugs

--- include/FeatureProcessor.hpp
+++ include/FeatureProcessor.hpp
@@ -15,10 +15,10 @@ class FeatureProcessor {
   explicit FeatureProcessor();
   virtual ~FeatureProcessor();
   // or extractMatch()? put those two in the same function
-  bool extract(const std::unique_ptr<cv::Mat>& image_seq, rawSIFT); 
-  bool match(rawSIFT, FeatureStruct& feature_struct);
+  // bool extract(const std::unique_ptr<cv::Mat>& image_seq, rawSIFT); 
+  // bool match(rawSIFT, FeatureStruct& feature_struct);
   bool skeletonize(Eigen::SparseMatrix<int, Eigen::RowMajor>& skeleton);
-}
+};
 
 } //namespace sparse_batch_sfm
 
