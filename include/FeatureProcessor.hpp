774559b9a9e9876d9225ed4478d1e4f300269cd9
rangozhang <rangozhangyu@gmail.com>
2016-12-06 01:38:07 -0500

    revision on featureProcessor

--- include/FeatureProcessor.hpp
+++ include/FeatureProcessor.hpp
@@ -12,9 +12,6 @@ namespace sparse_batch_sfm {
 
 class FeatureProcessor {
  public:
-  explicit FeatureProcessor();
-  virtual ~FeatureProcessor();
-  // or extractMatch()? put those two in the same function
   bool feature_match(const std::vector<std::unique_ptr<cv::Mat>>& image_seq, FeatureStruct& feature_struct, int minHessian, float match_thres, bool visualize); 
   bool skeletonize(Eigen::Matrix<int, Eigen::Dynamic,
                    Eigen::Dynamic, Eigen::RowMajor>& skeleton, float min_n_matches);
