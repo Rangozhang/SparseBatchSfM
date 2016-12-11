39688e44f259925883b36f56f594d43044c4093a
allenyangyl <allenyangyl@gmail.com>
2016-12-10 23:49:14 -0500

    almost complete merging update

--- src/GraphMerge.cpp
+++ src/GraphMerge.cpp
@@ -46,24 +46,59 @@ namespace sparse_batch_sfm {
       }
 
       // find whether a point in graphB is seen in graphA
+      std::vector<Eigen::Triplet<int>> triplet;
+      std::vector<int> unseen_idx;
+      for (int i = 0; i < graphA.feature_idx.outerSize(); i++) {
+        for (Eigen::SparseMatrix<int, Eigen::RowMajor>::InnerIterator it(graphA.feature_idx, i); it; ++it) {
+          triplet.push_back(Eigen::Triplet<int>(it.row(), it.col(), it.value()));
+        }
+      }
+      /*
+      for (int i = 0; i < triplet.size(); i++) {
+        std::cout << triplet[i].row() << " " << triplet[i].col() << " " << triplet[i].value() << std::endl;
+      }
+      */
       int num_point2 = graphB.feature_idx.cols();
       int count = 0;
       for (int i = 0; i < num_point2; i++) {
-        int point_ind = graphB.feature_idx.coeff(commonFrameIdx2_, i);
-        if (point_ind > 0) {
+        int point_ind2 = graphB.feature_idx.coeff(commonFrameIdx2_, i);
+        if (point_ind2 > 0) {
           point_2d this_point;
-          this_point.x = graphB.feature_points[point_ind - 1].pos(0);
-          this_point.y = graphB.feature_points[point_ind - 1].pos(1);
-          std::unordered_map<point_2d, int>::const_iterator seen = hash_point.find(this_point);
-          if (seen == hash_point.end()) {
+          this_point.x = graphB.feature_points[point_ind2 - 1].pos(0);
+          this_point.y = graphB.feature_points[point_ind2 - 1].pos(1);
+          if (hash_point.count(this_point)) {
+            graphA.feature_points.push_back(graphB.feature_points[point_ind2 - 1]);
+            triplet.push_back(Eigen::Triplet<int>(graphA.frame_idx.size(), hash_point[this_point], graphA.feature_points.size()));
             count++;
           }
           else {
-
+            int point_ind1 = graphB.feature_idx.coeff(newFrameIdx_, i);
+            graphA.feature_points.push_back(graphB.feature_points[point_ind1 - 1]);
+            triplet.push_back(Eigen::Triplet<int>(commonFrameIdx1_, num_point1, graphA.feature_points.size()));
+            graphA.feature_points.push_back(graphB.feature_points[point_ind2 - 1]);
+            triplet.push_back(Eigen::Triplet<int>(graphA.frame_idx.size(), num_point1, graphA.feature_points.size()));
+            unseen_idx.push_back(i);
+            num_point1++;
           }
         }
       }
-      std::cout << num_point2 - count << "/" << num_point2 << std::endl;
+      graphA.feature_idx.resize(graphA.frame_idx.size() + 1, num_point1);
+      graphA.feature_idx.setFromTriplets(triplet.begin(), triplet.end());
+      Eigen::Matrix<double, 6, Eigen::Dynamic> Str_temp = graphA.Str;
+      graphA.Str.resize(6, num_point1);
+      graphA.Str.leftCols(Str_temp.cols()) = Str_temp;
+      for (int i = 0; i < unseen_idx.size(); i++) {
+        graphA.Str.col(Str_temp.cols() + i) = graphB.Str.col(unseen_idx[i]);
+      }
+      /*
+      for (int i = 0; i < triplet.size(); i++) {
+        std::cout << triplet[i].row() << " " << triplet[i].col() << " " << triplet[i].value() << std::endl;
+      }
+      for (int i = 0; i < unseen_idx.size(); i++) {
+        std::cout << unseen_idx[i] << std::endl;
+      }
+      */
+      std::cout << "Number of common points " << count << "/" << num_point2 << std::endl;
 
       for (int i = 0; i < graphA.frame_idx.size(); i++) {
         std::cout << graphA.frame_idx[i] << std::endl;
